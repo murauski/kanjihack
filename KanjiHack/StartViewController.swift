3e8f2ad74723ba520539da343c939a41418a3d47
Anthony Marchenko <anthonymarchenko@gmail.com>
2018-04-28 09:40:14 +0300

    project init

--- KanjiHack/StartViewController.swift
+++ KanjiHack/StartViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func startButtonDidPressed(_ sender: UIButton) {
+        let mainViewController = storyboard?.instantiateViewController(withIdentifier: "questionViewControllerKey") as! QuestionsViewController
+        navigationController?.pushViewController(questionViewController,
+                                                 animated: true)
+    }
+    
+    
 }
 
