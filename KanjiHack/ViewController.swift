5697bb3ead5280576119297da9c8224fe07d767d
Anthony Marchenko <anthonymarchenko@gmail.com>
2018-04-28 09:29:50 +0300

    Initial Commit

---
+++ KanjiHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KanjiHack
+//
+//  Created by Anthony Marchenko on 4/28/18.
+//  Copyright © 2018 Anthony Marchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
